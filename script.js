2ed783f4e7c8f8c264191d1f9be6c8c7f50b448a
Amathost <124377028+Amathost@users.noreply.github.com>
2024-09-28 15:42:54 -0400

    reformated script into header.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
