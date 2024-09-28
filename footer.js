e6d53be0d9dd46f55b05b47b5da9c634f9e3faaf
Amathost <124377028+Amathost@users.noreply.github.com>
2024-09-28 15:34:58 -0400

    created footer header scripts and template

---
+++ footer.js
@@ -0,0 +1,25 @@
+const footer = document.getElementsByClassName("footer")[0];
+
+footer.innerHTML = `
+    <div class="social-media">
+        <a href="#"
+        ><img
+                src="https://img.icons8.com/color/48/000000/facebook.png"
+                alt="Facebook"
+        /></a>
+        <a href="#"
+        ><img
+                src="https://img.icons8.com/ios-filled/50/000000/x.png"
+                alt="X (formerly Twitter)"
+        /></a>
+        <a href="#"
+        ><img
+                src="https://img.icons8.com/color/48/000000/instagram-new.png"
+                alt="Instagram"
+        /></a>
+    </div>
+    <p>
+        &copy; 2024 ChaChingAcademy. Empowering communities through financial
+        education.
+    </p>
+`
