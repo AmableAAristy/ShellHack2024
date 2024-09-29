30186dc01299c12fbd20b6c8ea05bea3952e5e38
Amathost <124377028+Amathost@users.noreply.github.com>
2024-09-29 02:19:38 -0400

    fixed footer link

--- public/footer.js
+++ public/footer.js
@@ -2,17 +2,17 @@ const footer = document.getElementsByClassName("footer")[0];
 
 footer.innerHTML = `
     <div class="social-media">
-        <a href="#"
+        <a href="https://www.facebook.com" target="_blank"
         ><img
                 src="https://img.icons8.com/color/48/000000/facebook.png"
                 alt="Facebook"
         /></a>
-        <a href="#"
+        <a href="https://www.twitter.com" target="_blank" 
         ><img
                 src="https://img.icons8.com/ios-filled/50/000000/x.png"
                 alt="X (formerly Twitter)"
         /></a>
-        <a href="#"
+        <a href="https://www.instagram.com" target="_blank"
         ><img
                 src="https://img.icons8.com/color/48/000000/instagram-new.png"
                 alt="Instagram"
