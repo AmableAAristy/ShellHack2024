ddfd159351a9d28e122420c8cdb7e074263d9537
Amathost <124377028+Amathost@users.noreply.github.com>
2024-09-29 01:11:09 -0400

    gemini works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const stockDataRoute = require('./routes/stockData');
+const geminiRoute = require('./routes/geminiroute');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/stockdata', stockDataRoute);
 
+
+app.use(express.json());
+
+app.use('/', geminiRoute);  
+
+
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+
